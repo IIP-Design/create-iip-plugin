065af99df54399e7ef259b254811f3b4d3f2e20b
mrewers <mrewers@users.noreply.github.com>
2018-11-02 12:57:38 -0400

    Move app functions from index to run.js

--- index.js
+++ index.js
@@ -1,27 +1,12 @@
 #!/usr/bin/env node
 
-const chalk = require( 'chalk' );
-const clear = require( 'clear' );
-const figlet = require( 'figlet' );
+'use strict';
 
-const files = require( './lib/files' );
-const inquirer = require( './lib/inquirer' );
-const repo = require( './lib/repo' );
+// Ends application on unhandled rejections
+process.on( 'unhandledRejection', err => {
+	throw err;
+} );
 
-clear();
-
-console.log(
-  chalk.green(
-    figlet.textSync( 'Create IIP Plugin', {
-      font: 'Big Money-ne',
-      horizontalLayout: 'default'
-    })
-  )
-)
-
-const run = async () => {
-  const specs = await inquirer.askPluginSpecs();
-  console.log(specs);
-}
-
+// Run the entire program with async/await functions.
+const run = require( './lib/run' );
 run();
